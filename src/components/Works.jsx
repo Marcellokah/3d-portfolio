64b72f37c9b327d42f4bfddfd35bd311576051e6
Marcellokah <aalma47@gmail.com>
2024-01-16 10:10:29 +0100

    Update App.jsx and Works.jsx

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -39,13 +39,13 @@ const Works = () => {
   return (
     <>
       <motion.div variants={textVariant()}>
-        <p className={styles.sectionSubText}>My works</p>
+        <p className={styles.sectionSubText}>My applications</p>
         <h2 className={styles.sectionHeadText}>Projects.</h2>
       </motion.div>
 
       <div className="w-full flex">
         <motion.div variants={fadeIn("", "", 0.1, 1)} className="mt-3 text-secondary text-[17px] max-w-3xl leading-[30px]">
-          In my projects showcase, you will find a variety of interactive applications that showcase my skills in JavaScript, HTML Canvas, and React. Many of my projects involve creating engaging games using HTML Canvas, which allows me to combine my passion for programming with my love of gaming. From classic games like Ping-Pong and Snake to more complex games like platformers and fighting games, I enjoy using my creativity and problem-solving skills to bring these games to life. In addition to game development, I have also worked on several React applications, including e-commerce sites and other interesting platforms. My projects showcase my ability to work independently and as part of a team, as well as my dedication to delivering high-quality products that exceed expectations.
+          In my projects showcase, you will find a variety of interactive applications that showcase my Front-End skills. Many of my projects involve creating engaging games using HTML Canvas, which allows me to combine my passion for programming with my love of gaming. From classic games like Ping-Pong and Snake to more complex games like platformers and fighting games, I enjoy using my creativity and problem-solving skills to bring these games to life. In addition to game development, I have also worked on several React applications, including e-commerce sites and other interesting platforms. My projects showcase my ability to work independently and as part of a team, as well as my dedication to delivering high-quality products that exceed expectations.
         </motion.div>
       </div>
 
